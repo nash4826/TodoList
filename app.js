19f81b9a87bf4b5caefd08e648c825352819f861
nash4826 <gys4826@gmail.com>
2022-05-09 21:21:53 +0900

    final project

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const _ = require('lodash');
-const bodyParser = require('body-Parser');
 const app = express();
 
 
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
@@ -91,6 +90,11 @@ app.post('/delete', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("Server Started on port 3000");
 })
